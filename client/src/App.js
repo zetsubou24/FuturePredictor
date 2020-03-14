24f1d499feeed273a128cca9ae365974651a7513
zetsubou24 <alphabetaomega4@gmail.com>
2020-03-14 08:30:50 +0300

    Add Appbar and Sidebars

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,120 @@
-import React from "react";
+import React, {
+  useState,
+  forwardRef,
+  useRef,
+  useImperativeHandle
+} from "react";
 import Map from "./Components/Map.jsx";
 import "semantic-ui-css/semantic.min.css";
-
+import {
+  Header,
+  Icon,
+  Image,
+  Menu,
+  Segment,
+  Sidebar,
+  Button
+} from "semantic-ui-react";
 function App() {
+  const childRef = useRef();
+  const [visible, setVisible] = useState(false);
+  const [activeItem, setActiveItem] = useState("Actions");
+  const handleItemClick = () => {
+    setVisible(!visible);
+    console.log(visible);
+  };
   return (
     <>
-      <div id="map" style={({ width: 10 }, { height: 800 })}></div>
+      <Segment inverted>
+        <Menu inverted pointing secondary>
+          <Menu.Item
+            name="Actions"
+            active={activeItem === "Actions"}
+            onClick={() => {
+              setActiveItem("Actions");
+              handleItemClick();
+            }}
+          />
+          <Menu.Item
+            name="Get Info"
+            active={activeItem === "Get Info"}
+            onClick={() => {
+              setActiveItem("Get Info");
+              handleItemClick();
+            }}
+          />
+        </Menu>
+      </Segment>
+      <Sidebar.Pushable as={Segment}>
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          direction="left"
+          icon="labeled"
+          inverted
+          onHide={() => setVisible(false)}
+          vertical
+          visible={visible}
+          width="thin"
+        >
+          <Menu.Item
+            as="a"
+            onClick={() => {
+              childRef.current.handleApiCallWrapper();
+              handleItemClick();
+            }}
+          >
+            Get Future Possible Location
+          </Menu.Item>
+          <Menu.Item
+            as="a"
+            onClick={() => {
+              childRef.current.findNearbyShoppingCentersWrapper();
+              handleItemClick();
+            }}
+          >
+            Find Nearby ShoppingCenters
+          </Menu.Item>
+          <Menu.Item
+            as="a"
+            onClick={() => {
+              childRef.current.findNearbyRestaurantsWrapper();
+              handleItemClick();
+            }}
+          >
+            Find Nearby Restaurants
+          </Menu.Item>
+          <Menu.Item
+            as="a"
+            onClick={() => {
+              childRef.current.findNearbyHospitalsWrapper();
+              handleItemClick();
+            }}
+          >
+            Find Nearby Hospitals
+          </Menu.Item>
+        </Sidebar>
+
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          direction="right"
+          inverted
+          vertical
+          visible={visible}
+        >
+          <Menu.Item as="a" header>
+            Points of Interest Info And Routes
+          </Menu.Item>
+          <Menu.Item as="a">Shopping Center Info</Menu.Item>
+        </Sidebar>
+
+        <Sidebar.Pusher>
+          <div id="map" style={({ width: 10 }, { height: 800 })}></div>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
       <div>
-        <Map />
+        <Map ref={childRef} />
       </div>
     </>
   );
--- client/src/Components/Map.jsx
+++ client/src/Components/Map.jsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState,
+  forwardRef,
+  useRef,
+  useImperativeHandle
+} from "react";
 import { Button } from "semantic-ui-react";
 
 function toFixed(num, fixed) {
@@ -6,7 +12,7 @@ function toFixed(num, fixed) {
   return num.toString().match(re)[0];
 }
 
-const Map = () => {
+const Map = forwardRef((props, ref) => {
   const [flag, setFlag] = useState(false);
   const [fg, setFg] = useState({});
   const [isDataUpdated, updateData] = useState(0);
@@ -14,30 +20,41 @@ const Map = () => {
     Latitude: toFixed(40.4212216, 5),
     Longitude: toFixed(-3.6286935, 5)
   });
-  const [shoppingCenters, setShoppingCenters] = useState({})
+  const [shoppingCenters, setShoppingCenters] = useState({});
+  useImperativeHandle(ref, () => ({
+    handleApiCallWrapper() {
+      handleApiCall();
+    },
+    findNearbyShoppingCentersWrapper() {
+      findNearbyShoppingCenters();
+    },
+    findNearbyRestaurantsWrapper() {
+      findNearbyRestaurants();
+    },
+    findNearbyHospitalsWrapper() {
+      findNearbyHospitals();
+    }
+  }));
   useEffect(() => {
-    window.L.mapquest.key = "6kGGFBuABs2Z9TqeYxq7GTxpgA3N9Qeg"
+    window.L.mapquest.key = "6kGGFBuABs2Z9TqeYxq7GTxpgA3N9Qeg";
     const newFg = window.L.featureGroup();
     setFg(newFg);
-  
-      window.L.mapquest.map("map", {
-        center: [toFixed(40.4212216, 5), toFixed(-3.6286935, 5)],
-        layers: [window.L.mapquest.tileLayer("dark"), newFg],
-        zoom: 15
-        });
 
-    window.L.marker(
-      [toFixed(40.4212216, 5), toFixed(-3.6286935, 5)],
-      {
-        icon: window.L.mapquest.icons.marker({
-          primaryColor: "#101820",
-          secondaryColor: "#417505",
-          shadow: true,
-          size: "md"
-        })
-      }
-    ).addTo(newFg); 
-  },[])
+    window.L.mapquest.map("map", {
+      center: [toFixed(40.4212216, 5), toFixed(-3.6286935, 5)],
+      layers: [window.L.mapquest.tileLayer("dark"), newFg],
+      zoom: 15
+    });
+
+    window.L.marker([toFixed(40.4212216, 5), toFixed(-3.6286935, 5)], {
+      icon: window.L.mapquest.icons.marker({
+        primaryColor: "#101820",
+        secondaryColor: "#417505",
+        shadow: true,
+        size: "md"
+      })
+    }).addTo(newFg);
+  }, []);
 
   const handleClickAdd = () => {
     window.L.marker(
@@ -63,161 +80,189 @@ const Map = () => {
         setFuture(data);
         console.log(data.Latitude, typeof data.Latitude);
         var directions = window.L.mapquest.directions();
-        directions.route({
-        start: [toFixed(40.4212216, 5), toFixed(-3.6286935, 5)],
-        end: [data.Latitude, data.Longitude]
-        },addCustomLayer);
-        function addCustomLayer(err, response){
-        var customLayer = window.L.mapquest.directionsLayer({
-          startMarker: {
-            icon: 'marker',
-            iconOptions: {
-              size: 'md',
-              primaryColor: "#101820",
-              secondaryColor: "#417505",
+        directions.route(
+          {
+            start: [toFixed(40.4212216, 5), toFixed(-3.6286935, 5)],
+            end: [data.Latitude, data.Longitude]
+          },
+          addCustomLayer
+        );
+        function addCustomLayer(err, response) {
+          var customLayer = window.L.mapquest.directionsLayer({
+            startMarker: {
+              icon: "marker",
+              iconOptions: {
+                size: "md",
+                primaryColor: "#101820",
+                secondaryColor: "#417505"
+              },
+              draggable: false,
+              title: "You are here"
             },
-            draggable: false,
-            title: 'You are here'
-          },
-          endMarker: {
-            icon: 'marker',
-            iconOptions: {
-              size: 'md',
-              primaryColor: "#101820",
-              secondaryColor: "#800000",
+            endMarker: {
+              icon: "marker",
+              iconOptions: {
+                size: "md",
+                primaryColor: "#101820",
+                secondaryColor: "#800000"
+              },
+              title: "Possible destination"
             },
-            title: 'Possible destination'
-          },
-          routeRibbon: {
-            color: "#2aa6ce",
-            opacity: 1.0,
-            showTraffic: false,
-          },
-          directionsResponse: response
-        });
-        customLayer.addTo(fg);
-      }
+            routeRibbon: {
+              color: "#2aa6ce",
+              opacity: 1.0,
+              showTraffic: false
+            },
+            directionsResponse: response
+          });
+          customLayer.addTo(fg);
+        }
         updateData(isDataUpdated + 1);
       });
   };
 
   const findNearbyRestaurants = () => {
     const TomtomKEY = "2xrfCdFd2nGVXM5iYXAZpueDTqBfFYIh";
-    fetch(`https://api.tomtom.com/search/2/poiSearch/restaurant.json?limit=4&lat=${future.Latitude}&lon=${future.Longitude}&radius=1000&categorySet=7315&key=${TomtomKEY}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      for(var i in data.results){
-        console.log(data.results[i].position.lat, data.results[i].position.lon);
-        window.L.marker(
-          [toFixed(data.results[i].position.lat, 5), toFixed(data.results[i].position.lon, 5)],
-          {
-            icon: window.L.mapquest.icons.marker({
-              primaryColor: "#0e2433",
-              secondaryColor: "#0e2433",
-              shadow: true,
-              size: "md",
-              symbol: "R"
-            },
-          
-            ),
-            title: data.results[i].poi.name
-          }
-        ).addTo(fg);
-      }
-    }) 
-  }
+    fetch(
+      `https://api.tomtom.com/search/2/poiSearch/restaurant.json?limit=4&lat=${future.Latitude}&lon=${future.Longitude}&radius=1000&categorySet=7315&key=${TomtomKEY}`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        for (var i in data.results) {
+          console.log(
+            data.results[i].position.lat,
+            data.results[i].position.lon
+          );
+          window.L.marker(
+            [
+              toFixed(data.results[i].position.lat, 5),
+              toFixed(data.results[i].position.lon, 5)
+            ],
+            {
+              icon: window.L.mapquest.icons.marker({
+                primaryColor: "#0e2433",
+                secondaryColor: "#0e2433",
+                shadow: true,
+                size: "md",
+                symbol: "R"
+              }),
+              title: data.results[i].poi.name
+            }
+          ).addTo(fg);
+        }
+      });
+  };
 
   const findNearbyHospitals = () => {
     const TomtomKEY = "2xrfCdFd2nGVXM5iYXAZpueDTqBfFYIh";
-    fetch(`https://api.tomtom.com/search/2/poiSearch/.json?limit=4&lat=${future.Latitude}&lon=${future.Longitude}&radius=10000&categorySet=7321&key=${TomtomKEY}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      for(var i in data.results){
-        console.log(data.results)
-        console.log(data.results[i].position.lat, data.results[i].position.lon);
-        window.L.marker(
-          [toFixed(data.results[i].position.lat, 5), toFixed(data.results[i].position.lon, 5)],
-          {
-            icon: window.L.mapquest.icons.marker({
-              primaryColor: "#ff0000",
-              secondaryColor: "#ff0000",
-              shadow: true,
-              size: "md",
-              symbol: "H"
-            }),
-            title: data.results[i].poi.name
-          }, 
-        ).addTo(fg);
-      }
-    }) 
-  }
+    fetch(
+      `https://api.tomtom.com/search/2/poiSearch/.json?limit=4&lat=${future.Latitude}&lon=${future.Longitude}&radius=10000&categorySet=7321&key=${TomtomKEY}`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        for (var i in data.results) {
+          console.log(data.results);
+          console.log(
+            data.results[i].position.lat,
+            data.results[i].position.lon
+          );
+          window.L.marker(
+            [
+              toFixed(data.results[i].position.lat, 5),
+              toFixed(data.results[i].position.lon, 5)
+            ],
+            {
+              icon: window.L.mapquest.icons.marker({
+                primaryColor: "#ff0000",
+                secondaryColor: "#ff0000",
+                shadow: true,
+                size: "md",
+                symbol: "H"
+              }),
+              title: data.results[i].poi.name
+            }
+          ).addTo(fg);
+        }
+      });
+  };
 
   const findNearbyShoppingCenters = () => {
     const TomtomKEY = "2xrfCdFd2nGVXM5iYXAZpueDTqBfFYIh";
-    fetch(`https://api.tomtom.com/search/2/poiSearch/.json?limit=4&lat=${future.Latitude}&lon=${future.Longitude}&radius=10000&categorySet=7373&key=${TomtomKEY}`)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      for(var i in data.results){
-        console.log(data.results)
-        console.log(data.results[i].position.lat, data.results[i].position.lon);
-        // window.L.marker(
-        //   [toFixed(data.results[i].position.lat, 5), toFixed(data.results[i].position.lon, 5)],
-        //   {
-        //     icon: window.L.mapquest.icons.marker({
-        //       primaryColor: "#ffa500",
-        //       secondaryColor: "#ffa500",
-        //       shadow: true,
-        //       size: "md",
-        //       symbol: "S"
-        //     }),
-        //     title: data.results[i].poi.name
-        //   }, 
-        // ).addTo(fg);
-        var directions = window.L.mapquest.directions();
-        directions.route({
-        start: [toFixed(40.4212216, 5), toFixed(-3.6286935, 5)],
-        end:[toFixed(data.results[i].position.lat, 5), toFixed(data.results[i].position.lon, 5)]
-        },addCustomLayer);
-        function addCustomLayer(err, response){
-        var customLayer = window.L.mapquest.directionsLayer({
-          startMarker: {
-            icon: 'marker',
-            iconOptions: {
-              size: 'md',
-              primaryColor: "#101820",
-              secondaryColor: "#417505",
+    fetch(
+      `https://api.tomtom.com/search/2/poiSearch/.json?limit=4&lat=${future.Latitude}&lon=${future.Longitude}&radius=10000&categorySet=7373&key=${TomtomKEY}`
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        for (var i in data.results) {
+          console.log(data.results);
+          console.log(
+            data.results[i].position.lat,
+            data.results[i].position.lon
+          );
+          // window.L.marker(
+          //   [toFixed(data.results[i].position.lat, 5), toFixed(data.results[i].position.lon, 5)],
+          //   {
+          //     icon: window.L.mapquest.icons.marker({
+          //       primaryColor: "#ffa500",
+          //       secondaryColor: "#ffa500",
+          //       shadow: true,
+          //       size: "md",
+          //       symbol: "S"
+          //     }),
+          //     title: data.results[i].poi.name
+          //   },
+          // ).addTo(fg);
+          var directions = window.L.mapquest.directions();
+          directions.route(
+            {
+              start: [toFixed(40.4212216, 5), toFixed(-3.6286935, 5)],
+              end: [
+                toFixed(data.results[i].position.lat, 5),
+                toFixed(data.results[i].position.lon, 5)
+              ]
             },
-            draggable: false,
-            title: 'You are here'
-          },
-          endMarker: {
-            icon: 'marker',
-            iconOptions: {
-              size: 'md',
-              primaryColor: "#ffa500",
-              secondaryColor: "#ffa500",
-              symbol: "S"
-            },
-            title: data.results[i].poi.name,
-          },
-          routeRibbon: {
-            color: "#2aa6ce",
-            opacity: 1.0,
-            showTraffic: false,
-          },
-          directionsResponse: response
-        });
-        customLayer.addTo(fg);
-      }
-      }
-    }) 
-  }
+            addCustomLayer
+          );
+          function addCustomLayer(err, response) {
+            var customLayer = window.L.mapquest.directionsLayer({
+              startMarker: {
+                icon: "marker",
+                iconOptions: {
+                  size: "md",
+                  primaryColor: "#101820",
+                  secondaryColor: "#417505"
+                },
+                draggable: false,
+                title: "You are here"
+              },
+              endMarker: {
+                icon: "marker",
+                iconOptions: {
+                  size: "md",
+                  primaryColor: "#ffa500",
+                  secondaryColor: "#ffa500",
+                  symbol: "S"
+                },
+                title: data.results[i].poi.name
+              },
+              routeRibbon: {
+                color: "#2aa6ce",
+                opacity: 1.0,
+                showTraffic: false
+              },
+              directionsResponse: response
+            });
+            customLayer.addTo(fg);
+          }
+        }
+      });
+  };
 
   const handleClickRemove = () => {
     if (fg.getLayers().length > 0) fg.removeLayer(fg.getLayers()[0]);
@@ -241,19 +286,23 @@ const Map = () => {
     setFg(fg);
   };
 
-
   return (
     <>
-      <Button onClick={() => handleClickAdd()}>Add Marker</Button>
+      {/* <Button onClick={() => handleClickAdd()}>Add Marker</Button>
       <Button onClick={() => handleClickRemove()}>Remove Marker</Button>
       <Button onClick={() => handleClick()}>Toggle Marker</Button>
       <Button onClick={() => handleApiCall()}>Make API Call</Button>
-      <Button onClick={() => findNearbyRestaurants()}>Find Nearby Restaurants</Button>
-      <Button onClick={() => findNearbyHospitals()}>Find Nearby Hospital</Button>
-      <Button onClick={() => findNearbyShoppingCenters()}>Find Nearby Shopping Centers</Button>
-
+      <Button onClick={() => findNearbyRestaurants()}>
+        Find Nearby Restaurants
+      </Button>
+      <Button onClick={() => findNearbyHospitals()}>
+        Find Nearby Hospital
+      </Button>
+      <Button onClick={() => findNearbyShoppingCenters()}>
+        Find Nearby Shopping Centers
+      </Button> */}
     </>
   );
-};
+});
 
 export default Map;
